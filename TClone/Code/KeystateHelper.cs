c5b4a1cbd049416a7335741f6866e6e3f4a0f3cf
Sebastian Casper <sebastiancasper3@aol.com>
2017-12-20 15:53:02 -0500

    Add gamepad support to KeystateHelper

--- TClone/Code/KeystateHelper.cs
+++ TClone/Code/KeystateHelper.cs
@@ -4,19 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace TClone {
     static class KeystateHelper {
         public static KeyboardState state;
         static KeyboardState previous;
 
+        public static GamePadState buttonState;
+        private static GamePadState previousButtonState;
+
         public static void Update() {
             previous = state;
             state = Keyboard.GetState();
+
+            previousButtonState = buttonState;
+            buttonState = GamePad.GetState(PlayerIndex.One);
         }
 
         public static bool IsKeyReleased(Keys key) {
             return state.IsKeyUp(key) && previous.IsKeyDown(key);
+        }
+
+        public static bool IsButtonPressed(Buttons button)
+        {
+            return buttonState.IsButtonDown(button) && previousButtonState.IsButtonUp(button);
+        }
+
+        public static bool IsButtonReleased(Buttons button)
+        {
+            return buttonState.IsButtonUp(button) && previousButtonState.IsButtonDown(button);
         }
     }
 }
